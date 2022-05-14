63c67ffdf80e4c6016824450c115dbfe0e9dec66
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2022-05-14 20:26:43 -0300

    bootstrap.sh: add do_neovim function.
        This function install/upgrade the neovim plugins.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
 # Luciano Antonio Borguetti Faustino 14/05/2022
 # This is a idempotent script used to provision my entire developer environment.
-# Idempotent scripts can be called multiple times and each time it’s called, it will 
+# Idempotent scripts can be called multiple times and each time it’s called, it will
 # have the same effects on the system.
-# 
+#
 # Currently support only raspberry pi os (64 bits).
 
 set -e errexit
 set -u nounset
 
-function file_changes() {
+function file_changes(){
+
     diff "dotfiles/${*}" "${HOME}/${*}" > /dev/null 2>&1 || false
+
 }
 
+function file_exists(){
 
-function file_exists() {
     stat "${HOME}/${*}" > /dev/null 2>&1 || false
+
 }
 
 function do_dotfile_install(){
 
     _dirname=""
 
-    _dirname=$(dirname "${*}")
+    _dirname=$(dirname "${*}") || false
     mkdir --parents "${HOME}/${_dirname}" > /dev/null || false
     cp --force --backup "dotfiles/${*}" "${HOME}/${*}" > /dev/null 2>&1 || false
 
@@ -51,17 +54,22 @@ while IFS="" read -r dotfile; do
         fi
     fi
 done <<< "$(cd dotfiles && find . -type f)"
+
 }
 
-function package_exists() {
-    dpkg --status "${@}" > /dev/null 2>&1
+function package_exists(){
+
+    dpkg --status "${@}" > /dev/null 2>&1 || false
+
 }
 
-function do_package_install() {
+function do_package_install(){
+
     DEBIAN_FRONTEND=noninteractive apt-get --yes --quiet install "${@}" > /dev/null || false
+
 }
 
-function do_packages() {
+function do_packages(){
 
 package=""
 
@@ -78,5 +86,13 @@ done < ./packages.txt
 
 }
 
+function do_neovim(){
+
+    vim +PlugUpdate +qall || false
+    vim +PlugUpgrade +qall || false
+
+}
+
 do_dotfiles
 do_packages
+do_neovim
