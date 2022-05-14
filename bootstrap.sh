872678ba7cc2a8fbc4392ffd6085f11f30abc9f9
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2022-05-14 18:57:15 -0300

    bootstrap.sh: add do_dotfiles function.
        Now you can add some config files in the folder ./dotfiles and
        this script will be copy to the home of the user.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,19 +9,64 @@
 set -e errexit
 set -u nounset
 
+function file_changes() {
+    diff "dotfiles/${*}" "${HOME}/${*}" > /dev/null 2>&1 || false
+}
+
+
+function file_exists() {
+    stat "${HOME}/${*}" > /dev/null 2>&1 || false
+}
+
+function do_dotfile_install(){
+
+    _dirname=""
+
+    _dirname=$(dirname "${*}")
+    mkdir --parents "${HOME}/${_dirname}" > /dev/null || false
+    cp --force --backup "dotfiles/${*}" "${HOME}/${*}" > /dev/null 2>&1 || false
+
+}
+
+function do_dotfiles(){
+
+dotfile=""
+
+while IFS="" read -r dotfile; do
+    if ! file_exists "${dotfile}"; then
+        echo -n "Ensuring the copy of the ${dotfile} dotfile: "
+        if ! do_dotfile_install "${dotfile}"; then
+            echo "fail"
+        else
+            echo "ok"
+        fi
+    else
+        if ! file_changes "${dotfile}"; then
+            echo -n "The file was changed, ensuring the copy of the ${dotfile} dotfile: "
+            if ! do_dotfile_install "${dotfile}"; then
+                echo "fail"
+            else
+                echo "ok"
+            fi
+        fi
+    fi
+done <<< "$(cd dotfiles && find . -type f)"
+}
+
 function package_exists() {
-	dpkg --status "${@}" > /dev/null 2>&1 
+    dpkg --status "${@}" > /dev/null 2>&1
 }
 
 function do_package_install() {
-	DEBIAN_FRONTEND=noninteractive apt-get --yes --quiet install "${@}" > /dev/null || false
+    DEBIAN_FRONTEND=noninteractive apt-get --yes --quiet install "${@}" > /dev/null || false
 }
 
 function do_packages() {
 
 package=""
+
 while IFS="" read -r package; do
-    if ! package_exists "${package}"; then 
+    if ! package_exists "${package}"; then
         echo -n "Ensuring the installation of the ${package} package: "
         if ! do_package_install "${package}"; then
             echo "fail"
@@ -33,4 +78,5 @@ done < ./packages.txt
 
 }
 
+do_dotfiles
 do_packages
